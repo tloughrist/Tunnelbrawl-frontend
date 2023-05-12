43b48f7b746d5be95de7409548d9ca718a85dd14
timloughrist <tim.loughrist@gmail.com>
2023-05-12 17:01:34 -0500

    start loggedin as false

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export { LoggedInContext, UserContext };
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState("unchecked");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [user, _setUser] = useState({});
   const [logNav, setLogNav] = useState(false);
   const navigate = useNavigate();
